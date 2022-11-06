22595480d165792df8c7ea8c9b663a2a8b11ea16
domagoj.vukovic <off.vukovic@gmail.com>
2022-11-06 12:30:22 +0100

    feat: add prettier and reformat

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type { import('@types/prettier').Config } */
+const config = {
+    endOfLine: 'lf',
+    printWidth: 130,
+    proseWrap: 'always',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+}
+
+module.exports = config
